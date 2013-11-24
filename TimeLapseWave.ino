97f5963440b33530563c01e9d08d5ee07e6385d7
Matt Gabriel <matt.g.martire@gmail.com>
2013-11-24 17:36:56 +0000

    Add button selector to TimeLapseWave.ino

--- TimeLapseWave.ino
+++ TimeLapseWave.ino
@@ -1,5 +1,6 @@
 #include <Servo.h> 
 Servo myservo;  // create servo object to control a servo 
+const int button = 13; //we are attaching a button to pin 13 to choose which shooting mode we'll use
  
 //Define wave variables
 float x = 0.1; // x starting point (I don't want to start at 0 and have anyone divide by 0 to avoid errors)
@@ -11,14 +12,29 @@ int coeficient= 8; // sin will be elevated to this
  
 void setup() { 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  pinMode(button,INPUT);
 } 
  
 void loop() { 
-  float val = (pow(sin(x/frequency),8) * amplitude + minValue) * 10; //calculate the Y axis (time)
-  val = val * 1000; //turn it into milliseconds
+  float val;
+  if(digitalRead(button) == 0){ //button off, select timeWaveMode
+    val = timeWaveMode();
+  } else {
+    val = timeFlatMode(); //button on, select timeFlatMode
+  }
   myservo.write(100); //rotate Servo to 100 degrees (press shutter)
   delay(300); //wait 0.3 seconds
   myservo.write(90); //rotate back to 90 degrees (release shutter)
   delay(val); //wait Y seconds
-  x += increment; //Increment x by 0.1  
 } 
+
+float timeWaveMode(){
+  float val = (pow(sin(x/frequency),8) * amplitude + minValue) * 10; //calculate the Y axis (time)
+  val = val * 1000; //turn it into milliseconds
+  x += increment; //Increment x by 0.1 
+  return val;
+}
+
+float timeFlatMode(){
+  return 5000.00; //will always return 5 seconds.
+}
